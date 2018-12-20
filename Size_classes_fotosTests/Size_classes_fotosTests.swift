b81c441e9e248488920b45fb2254f1e79c662328
celiavel <44905688+celiavel@users.noreply.github.com>
2018-12-20 20:38:50 +0100

    Add files via upload

---
+++ Size_classes_fotosTests/Size_classes_fotosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Size_classes_fotosTests.swift
+//  Size_classes_fotosTests
+//
+//  Created by Celia on 17/12/2018.
+//  Copyright © 2018 Celia. All rights reserved.
+//
+
+import XCTest
+@testable import Size_classes_fotos
+
+class Size_classes_fotosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
